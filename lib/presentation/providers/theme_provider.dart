6ec683c18515f9dd6af65ff45f6da908682f4e21
Hussein Tirawi <hussein.altirawi@foodics.com>
2026-01-24 19:42:08 +0300

    fix: Add missing SharedPreferences import to provider files

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../app/di/repository_providers.dart';
 import '../../core/theme/app_theme.dart';
--- lib/presentation/providers/user_preferences_provider.dart
+++ lib/presentation/providers/user_preferences_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../app/di/repository_providers.dart';
 
